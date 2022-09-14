516a947c9758ff754679a2d94c91cd212e129d59
Ghiiio <seunshenex95@gmail.com>
2022-09-13 17:41:29 -0700

    functions_nested_loops__

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
-  * main - print the word _putchar
-  *
-  * Return: 0 if successfu
-  */
+* main - Print the word "Holberton"
+*
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	int i;
 	char p[] = "_putchar";
 
-	for (i = 0; i < 9; i++)
-	{
+	for (i = 0; i < 8; i++)
+		{
 		_putchar(p[i]);
-	}
-	_putchar('\n');
+		}
+		_putchar('\n');
 	return (0);
 }
