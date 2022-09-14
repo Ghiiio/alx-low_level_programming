87d557647cbbc269eff9e62aaabe3c75216f7e73
Ghiiio <seunshenex95@gmail.com>
2022-09-13 17:12:34 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * main - print the word _putchar
+  *
+  * Return: 0 if successfu
+  */
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+	for (i = 0; i <= 8; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+	
+	return (0);
+}
