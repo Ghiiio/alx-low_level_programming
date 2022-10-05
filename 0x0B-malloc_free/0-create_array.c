2975acc466c634e61780a5ecdbd57d47dde97207
Ghiiio <seunshenex95@gmail.com>
2022-10-04 20:14:18 -0700

    memory allocation using malloc()

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * *create_array - creates an array of chars
+  *
+  * @size: size of array
+  * @c: character to be initialized
+  *
+  * Return: t
+  */
+char *create_array(unsigned int size, char c)
+{
+	char *t;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+
+	t = malloc(sizeof(char) * size);
+
+	if (t == NULL)
+		return (NULL);
+
+	while (i < size)
+	{
+		t[i] = c;
+		i++;
+	}
+	return (t);
+}
